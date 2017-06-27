0277fd5f0957345e8a919dcd2e5ece61fec279e0
Ryan Odening <party_on.garth@berkeley.edu>
2017-06-27 13:14:15 -0700

    migrate deckcards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627083246) do
+ActiveRecord::Schema.define(version: 20170627200934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170627083246) do
     t.string   "powerup"
     t.string   "name",       null: false
     t.string   "type",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "deck_cards", force: :cascade do |t|
+    t.integer  "deck_id",    null: false
+    t.integer  "card_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
