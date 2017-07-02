31895a2f3cc7ac1b5f36d9b14156c76c0d041572
Ryan Odening <party_on.garth@berkeley.edu>
2017-07-01 21:30:31 -0700

    set up root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,1 +1,26 @@
 import React from 'react'
+// see protectedrouter and object
+import { Provider } from 'react-redux'
+
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+
+import App from './app'
+
+const Root = ({ store }) => {
+
+  const _ensureLoggedIn = (nextState, replace) => {
+    if (!store.getState().session.currentUser)
+      replace('/')
+  }
+  //redirectIfLoggedIn
+
+  return (
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+
+        </Route>
+      </Router>
+    </Provider>
+  )
+}
